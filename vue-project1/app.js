359ea6fa3cda3e91b3e3d4c3327a9a37353ffab0
조준희 <junehi1015@naver.com>
2023-04-03 19:37:17 +0900

    methods

--- vue-project1/app.js
+++ vue-project1/app.js
@@ -5,6 +5,17 @@ const app = Vue.createApp({
       vueLink: 'https://vuejs.org/'
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+
+      if(randomNumber < 0.5) {
+        return 'Finish the course!';
+      }else {
+        return 'Learn Vue';
+      }
+    }
+  }
 });
 
 app.mount('#user-goal');
