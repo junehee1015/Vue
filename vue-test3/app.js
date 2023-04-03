7094cea4f503d437c571c1c26d98ca82e2c57dcf
조준희 <junehi1015@naver.com>
2023-04-04 00:30:12 +0900

    v-on / event

---
+++ vue-test3/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add(num) {
+      // this.counter++;
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      // this.counter--;
+      this.counter = this.counter - num;
+    }
+  }
+});
+
+app.mount('#events');
