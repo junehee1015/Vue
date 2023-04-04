37eab10742f68b035cd234d5381403ca4523fd2a
조준희 <junehi1015@naver.com>
2023-04-04 21:44:59 +0900

    이벤트 수식어

--- vue-test3/app.js
+++ vue-test3/app.js
@@ -2,10 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
+      confirmName: "",
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmName = this.name;
+    },
+    // 브라우저가 초기화되지 않는 방법 1
+    // submitForm(e) {
+    //   e.preventDefault();
+    // },
+
+    // 브라우저가 초기화되지 않는 방법 2(이벤트 수식어)
+    submitForm() {},
+
     setName(e, text) {
       this.name = e.target.value + text;
     },
@@ -14,8 +26,8 @@ const app = Vue.createApp({
     },
     reduce(num) {
       this.counter = this.counter - num;
-    }
-  }
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
