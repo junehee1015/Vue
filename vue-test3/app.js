cb90b75fa0814680609a0075c6063f2f17f4a1ef
조준희 <junehi1015@naver.com>
2023-04-04 21:14:33 +0900

    v-on:input / event인자

--- vue-test3/app.js
+++ vue-test3/app.js
@@ -2,15 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(e, text) {
+      this.name = e.target.value + text;
+    },
     add(num) {
-      // this.counter++;
       this.counter = this.counter + num;
     },
     reduce(num) {
-      // this.counter--;
       this.counter = this.counter - num;
     }
   }
