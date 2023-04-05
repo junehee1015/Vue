a0b427b3c98ee7f41633d076806c03892cac95b4
조준희 <junehi1015@naver.com>
2023-04-05 17:42:55 +0900

    computed

--- vue-test4-1/app.js
+++ vue-test4-1/app.js
@@ -5,6 +5,19 @@ const app = Vue.createApp({
       name: ''
     };
   },
+
+  // 종속 대상이 변경되기 전까지 호출되지 않는다.
+  computed: {
+    plus() {
+      console.log('running...');
+      if(this.name === '') {
+        return '';
+      }
+      return this.name + '입니다.'
+    },
+  },
+
+  // methods는 렌더링이 일어날 때 항상 method를 실행한다.
   methods: {
     setName(event, lastName) {
       this.name = event.target.value;
