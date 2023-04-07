ed90b99c62dcf115f6949a7014ec595d32142ce5
조준희 <junehi1015@naver.com>
2023-04-07 22:29:24 +0900

    heal / game over / game start

--- vue-project1/app.js
+++ vue-project1/app.js
@@ -2,9 +2,6 @@
 function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-function isHealth(health) {
-  return health < 0 ? 0 : health;
-}
 
 const app = Vue.createApp({
   data() {
@@ -12,21 +9,45 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       round: 0,
+      winner: null,
     };
   },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        this.winner = 'monster';
+      }
+    },
+    monsterHealth(value) {
+      if(value <= 0 && this.playerHealth <= 0) {
+        this.winner = 'draw';
+      }else if (value <= 0) {
+        this.winner = 'player';
+      }
+    },
+  },
+
   // HTML 로직은 모두 computed로
   computed: {
     monsterHealthBar() {
-      return { width: this.monsterHealth < 0 ? 0 : this.monsterHealth + '%' };
+      return { width: this.monsterHealth < 0 ? 0 : this.monsterHealth + "%" };
     },
     playerHealthBar() {
-      return { width: this.playerHealth < 0 ? 0 : this.playerHealth + '%' };
+      return { width: this.playerHealth < 0 ? 0 : this.playerHealth + "%" };
     },
     specialAttackBtn() {
       return this.round % 3 !== 0;
-    }
+    },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.round = 0;
+      this.winner = null;
+    },
     attackMonster() {
       // 세 번째 턴마다 specialAttack을 하기 위해 round 수를 알아야한다.
       this.round++;
@@ -34,23 +55,28 @@ const app = Vue.createApp({
       // 최소 ~ 최대 -> Math.floor(Math.random() * (최대 - 최소)) + 최소
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
-      this.monsterHealth = isHealth(this.monsterHealth);
-      
+
       // Monster를 공격하면 Player도 반격당해야하기 때문에 attackPlayer method를 호출해준다.
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
-      this.playerHealth = isHealth(this.playerHealth);
     },
     specialAttackMonster() {
       this.round++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
-    }
-  }
+    },
+    healPlayer() {
+      this.round++;
+      const healValue = getRandomValue(10, 20);
+      this.playerHealth += healValue;
+      this.playerHealth = this.playerHealth > 100 ? 100 : this.playerHealth;
+      this.attackPlayer();
+    },
+  },
 });
 
-app.mount('#game');
+app.mount("#game");
