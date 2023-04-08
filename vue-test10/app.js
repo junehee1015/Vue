1e3ee06eac3dc8226d9abcffb53b08e46a297c3a
조준희 <junehi1015@naver.com>
2023-04-09 01:13:58 +0900

    component(cli X)

--- vue-test10/app.js
+++ vue-test10/app.js
@@ -1,5 +1,54 @@
 const app = Vue.createApp({
-
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    
+  },
 });
 
-app.mount('#app');
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetail">{{ showDetail ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="showDetail">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      showDetail: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      }
+    };
+  },
+  methods: {
+    toggleDetail() {
+      this.showDetail = !this.showDetail;
+    },
+  }
+});
+
+app.mount("#app");
