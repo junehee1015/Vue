bf139a3326e5f7058bf5a35648593f8f756d8110
조준희 <junehi1015@naver.com>
2023-04-10 03:06:26 +0900

    vue-cli setting / Component간의 data 통신(Props)

---
+++ vue-cli-01/src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavoriteData === '1' ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleDetail">{{ visibleDetail ? 'Hide' : 'Show' }} Detail</button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="visibleDetail">
+      <li><strong>Phone: </strong> {{ phoneNumber }}</li>
+      <li><strong>email: </strong> {{ emailAdress }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAdress',
+    // 받아온 Props의 값은 변경할 수 없다.
+    // Vue는 단방향 데이터 플로우의 개념을 사용하기 때문이다.
+    // 그러나 다른 데이터에 넣어서 값을 변경하여 사용하는 것은 가능하다.
+    'isFavorite'
+  ],
+  data() {
+    return {
+      visibleDetail: false,
+      isFavoriteData: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetail() {
+      this.visibleDetail = !this.visibleDetail;
+    },
+    toggleFavorite() {
+      this.isFavoriteData = this.isFavoriteData === '1' ? '0' : '1';
+    }
+  }
+}
+</script>
