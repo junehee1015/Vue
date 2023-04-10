28656abbe8cbbc08dbc24c3843b9f3f6f7dee975
조준희 <junehi1015@naver.com>
2023-04-10 11:07:18 +0900

    Props 표현방법

--- vue-cli-01/src/components/FriendContact.vue
+++ vue-cli-01/src/components/FriendContact.vue
@@ -11,15 +11,32 @@
 </template>
 <script>
 export default {
-  props: [
-    'name',
-    'phoneNumber',
-    'emailAdress',
-    // 받아온 Props의 값은 변경할 수 없다.
-    // Vue는 단방향 데이터 플로우의 개념을 사용하기 때문이다.
-    // 그러나 다른 데이터에 넣어서 값을 변경하여 사용하는 것은 가능하다.
-    'isFavorite'
-  ],
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAdress',
+  //   // 받아온 Props의 값은 변경할 수 없다.
+  //   // Vue는 단방향 데이터 플로우의 개념을 사용하기 때문이다.
+  //   // 그러나 다른 데이터에 넣어서 값을 변경하여 사용하는 것은 가능하다.
+  //   'isFavorite'
+  // ],
+  props: {
+    name: String,         // 축약
+    phoneNumber: String,
+    
+    emailAdress: {        // 상세
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,                // data type
+      required: false,             // 필수인지 아닌지
+      default: '0',                // 기본값
+      validator: function(value) { // data 유효성 검사
+        return value === '1' || value === '0';
+      }
+    },
+  },
   data() {
     return {
       visibleDetail: false,
