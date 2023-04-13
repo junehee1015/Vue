14a1fe9890b4bf3b7bb8a9c46fc893a2efdcc4c1
조준희 <junehi1015@naver.com>
2023-04-14 01:39:14 +0900

    Vue Project2 Start

---
+++ vue-project2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      storedResource: [
+        {
+          id: 'official-guide',
+          title: 'Ofiicial Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style>
+
+</style>
