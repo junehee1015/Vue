d682894a6aaec93d8b4ba524f81148ad976c5c9c
조준희 <junehi1015@naver.com>
2023-04-20 00:37:41 +0900

    v-model / number

---
+++ vue-test11/src/components/TheForm.vue
@@ -0,0 +1,142 @@
+<template>
+  <form @submit.prevent="submiyForm">
+    <div class="form-control">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" v-model="userName"/>
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <!-- v-model은 input의 data type이 Number인지 확인 후 String에서 Number로 변환해준다. -->
+      <!-- v-model을 사용한 숫자 data는 Number type이지만 ref를 사용한 숫자 data는 String이다. -->
+
+      <!-- v-model.number=""를 사용하면 다른 type을 Number type으로 강제 변환할 수 있다. -->
+      <!-- 
+        v-model의 수식어
+        v-model.lazy : 바인딩한 property를 업데이트하는 방법을 바꿀 수 있다.
+        v-model.trim : 입력된 text의 앞과 뒤에 붙은 공백을 제거해준다.
+      -->
+      <input id="age" name="age" type="number" v-model="userAge"/>
+    </div>
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-news" name="interest" type="checkbox" />
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input id="interest-nothing" name="interest" type="checkbox" />
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" name="how" type="radio" />
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-blogs" name="how" type="radio" />
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input id="how-other" name="how" type="radio" />
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+    };
+  },
+  methods: {
+    submiyForm() {
+      
+    }
+  },
+}
+</script>
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
